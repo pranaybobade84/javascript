ddc13a0d7c602d8eb436d2ece32df1b18793d086
pranay bobade <pranaybobade84@gmail.com>
2025-01-01 14:55:33 +0530

    solved reverse no problem

--- loops/while/revreseNo.js
+++ loops/while/revreseNo.js
@@ -1,1 +1,17 @@
+// reverse the no
 
+let number = -26348;
+
+// If number is negative then convert to positive
+if (number < 0) {
+  number = Math.abs(number);
+}
+
+let reversedNo = 0;
+while (number > 0) {
+  let lastDigit = number % 10;
+  reversedNo = reversedNo * 10 + lastDigit;
+  number = Math.floor(number / 10);
+}
+
+console.log(reversedNo);
